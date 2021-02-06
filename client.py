981e7f3693b76a74899516d2845e3882715b1eb6
Panagiotis Lymperopoulos <panliberopoulos@gmail.com>
2021-02-06 14:31:55 -0500

    .

---
+++ client.py
@@ -0,0 +1,13 @@
+import socket
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+while True:
+    data = s.recv(1024)
+    if not data:
+        break
+    print(repr(data))
+
---
+++ test.py
@@ -0,0 +1,32 @@
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+
+import socket
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+# allow the camera to warmup
+time.sleep(0.1)
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 4000        # Port to listen on (non-privileged ports are > 1023)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen()
+conn, addr = s.accept()
+
+        
+
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw NumPy array representing the image, then initialize the timestamp
+	# and occupied/unoccupied text
+	image = frame.array
+    conn.sendall(image)
+	
